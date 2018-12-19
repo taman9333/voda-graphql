db0ac9e87500c3c3e3252a71e5ae7b7905a0ec2d
taman9333 <abdelrahman.taman@robustastudio.com>
2018-12-19 11:54:43 +0200

    add custom fields

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -8,6 +8,8 @@ module Types
     field :last_name, String, null: true
     field :yob, Int, null: false
     field :is_alive, Boolean, null: true
+    field :full_name, String, null: true
+    field :coordinates, Types::CoordinatesType, null: true
 
   end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,11 @@
 class Author < ApplicationRecord
+
+  def full_name
+    [first_name, last_name].compact.join ' '
+  end
+
+  def coordinates
+    [rand(1000), rand(2000)]
+  end
+
 end
