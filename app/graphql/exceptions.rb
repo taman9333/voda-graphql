ebbfc51ceb94b7cc3b7eaf011cc5638dd1d55b5d
taman9333 <abdelrahman.taman@robustastudio.com>
2018-12-24 21:07:11 +0200

    change how we include module instead of using activesupport concern going to add rescues on the included hook up

--- app/graphql/exceptions.rb
+++ app/graphql/exceptions.rb
@@ -1,10 +1,18 @@
 module Exceptions
   # require 'active_support/all'
-  extend ActiveSupport::Concern
+  # extend ActiveSupport::Concern
   
-  included do
-    rescue_from(ActiveRecord::RecordNotFound) do |error|
-      error.message
+  # included do
+  #   rescue_from(ActiveRecord::RecordNotFound) do |error|
+  #     error.message
+  #   end
+  # end
+
+  def self.included klass
+    klass.class_eval do
+      rescue_from(ActiveRecord::RecordNotFound) do |error|
+        error.message
+      end
     end
   end
 
